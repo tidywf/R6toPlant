f1916d155158d8c5b67931cc256ff7752aced3e2
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2024-03-04 10:18:51 +0000

    find internal-only classes too

--- R/get_all_classes.R
+++ R/get_all_classes.R
@@ -1,17 +1,23 @@
-get_R6_classes <- function(namespace){
+get_R6_classes <- function(namespace, internal=TRUE){
+    if(internal){
+        names = ls(getNamespace(namespace), all.names=TRUE)
+    }else{
+        names = getNamespaceExports(namespace)
+    }
+    
     r6classlist <- sort(
         names(Filter(
-            R6::is.R6Class, sapply(getNamespaceExports(namespace), get)
+            R6::is.R6Class, sapply(names, function(n){getFromNamespace(n, namespace)})
         )
         )
     )
 
-    r6classes <- sapply(r6classlist, get)
+    r6classes <- sapply(r6classlist, function(n){getFromNamespace(n, namespace)})
 
     return(r6classes)
 }
 
-make_package_diagram <- function(namespace, output=stdout(), method_args=TRUE){
-    clss = get_R6_classes(namespace)
+make_package_diagram <- function(namespace, output=stdout(), method_args=TRUE, internal=TRUE){
+    clss = get_R6_classes(namespace, internal=internal)
     make_plant(clss, output, method_args=method_args)
 }
