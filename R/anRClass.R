58adf2e8d37932130742c95a45ff0610709c9332
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2024-03-10 22:01:42 +0000

    clas testing

---
+++ R/anRClass.R
@@ -0,0 +1,13 @@
+anR6class = R6Class(
+    classname = "othername",
+    public = list(),
+    private = list()
+)
+
+anotherR6class = R6Class(
+    classname = "anothername",
+    public = list(),
+    private = list(),
+    inherit = anRclass
+)
+
